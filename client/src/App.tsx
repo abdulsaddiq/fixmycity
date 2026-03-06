19e6da9fd08da6f868f3bc58e788fcf25d2dfa8c
SHAIK ABDUL SADDIQ UMOOR BASHA <shaikabdulsadiqumoorbasha@gmail.com>
2026-03-06 16:30:35 +0530

    Create App.tsx

---
+++ client/src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: "40px", fontFamily: "sans-serif" }}>
+      <h1>FixMyCity</h1>
+      <p>AI City Problem Scanner</p>
+    </div>
+  );
+}
